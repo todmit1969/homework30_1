4603938462a9bcaa73ddb38ef4e52a88cbc702d5
tmitev <todor.mitev@sofairo.com>
2025-10-14 15:47:44 +0300

    Homework31 begining

---
+++ users/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework.permissions import BasePermission
+
+class IsModerator(BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.groups.filter(name='moderator').exists()
+
+
+class IsOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
-from users.models import Payment
+from users.models import Payment, CustomUser
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = '__all__'
+
+class CustomUserSerializer(ModelSerializer):
+
+    class Meta:
+        model =CustomUser
+        fields = "__all__"
