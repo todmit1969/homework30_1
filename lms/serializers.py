a7e0ef4cab2dc8f7c48d72fc350253f5450121d9
tmitev <todor.mitev@sofairo.com>
2025-10-20 16:02:22 +0300

    final corrections

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
@@ -10,12 +11,13 @@ class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
-        #validators = VideoLinkValidator(field='video_link')
+        validators = [VideoLinkValidator(field='video_link'),]
 
 
 class CourseSerializer(ModelSerializer):
     lessons = LessonSerializer(many=True, read_only=True)
     number_of_lessons = SerializerMethodField()
+    is_subscribed = serializers.SerializerMethodField()
 
     def get_number_of_lessons(self, course):
         return Lesson.objects.filter(course =course).count()
@@ -26,4 +28,4 @@ class CourseSerializer(ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ("id", "title", "preview", "description", "lessons", "number_of_lessons")
+        fields = "__all__" #("id", "title", "preview", "description", "lessons", "number_of_lessons")
